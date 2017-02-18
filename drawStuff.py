01a87e3d719abda93650fef16565f8f8298daf51
jdsouza1 <joeldsouza2008@gmail.com>
2017-02-18 11:19:28 -0500

    Add files via upload

---
+++ drawStuff.py
@@ -0,0 +1,13 @@
+import turtle
+
+
+def drawSquare():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+
+    window.exitonclick()
+
+drawSquare()
